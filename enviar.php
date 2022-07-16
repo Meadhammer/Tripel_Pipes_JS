c609996d71309c6aa8e041a17bbf1e6f2ab5b8a4
Julio Tripel <jftripel@gmail.com>
2022-07-16 19:28:56 -0300

    revision + php

---
+++ enviar.php
@@ -0,0 +1,29 @@
+<?php
+
+$nombre = $_POST["name"];
+$apellido = $_POST["surname"];
+$edad = $_POST["age"];
+$mail = $_POST["email"];
+$telefono = $_POST["phone"];
+$region = $_POST["zone"];
+$encuestaSi = $_POST["yes"];
+$encuestaNo = $_POST["no"];
+$mensaje = $_POST["mensaje"];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su edad es " . $edad . ",\r\n";
+$mensaje .= "Su e-mail es " . $mail . ",\r\n";
+$mensaje .= "Su teléfono es " . $telefono . ",\r\n";
+$mensaje .= "Su región es " . $region . ",\r\n";
+$mensaje .= "El resutado de la encuesta es " . $encuestaSi $encuestaNo . ",\r\n";
+$mensaje .= "Mensaje: " . $_POST["mensaje"] . ",\r\n";
+$mensaje .= "Enviado el: " . date("d/m/y", time());
+
+$para = "jftripel@gmail.com";
+$asunto = "Consulta desde TRIPEL PIPES";
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+
+?>
