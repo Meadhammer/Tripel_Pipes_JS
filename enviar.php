f01dc1b2319a29a72a6623df18ae09b8cb67cf62
Julio Tripel <jftripel@gmail.com>
2022-07-16 19:48:02 -0300

    3ra correct php

--- enviar.php
+++ enviar.php
@@ -6,17 +6,15 @@ $edad = $_POST["age"];
 $mail = $_POST["email"];
 $telefono = $_POST["phone"];
 $region = $_POST["zone"];
-$encuestaSi = $_POST["yes"];
-$encuestaNo = $_POST["no"];
 $mensaje = $_POST["mensaje"];
+
+$header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su edad es " . $edad . ",\r\n";
 $mensaje .= "Su e-mail es " . $mail . ",\r\n";
 $mensaje .= "Su teléfono es " . $telefono . ",\r\n";
 $mensaje .= "Su región es " . $region . ",\r\n";
-$mensaje .= "Ya he probado sus pipas " . $encuestaSi . ",\r\n";
-$mensaje .= "No he probado sus pipas " . $encuestaNo . ",\r\n";
 $mensaje .= "Mensaje: " . $_POST["mensaje"] . ",\r\n";
 $mensaje .= "Enviado el: " . date("d/m/y", time());
 
